62b6c0ea7c73c9a6c9d5378a8047012ff80107d7
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-05-15 11:02:18 -0500

    New update for my project

---
+++ components/Card.js
@@ -0,0 +1,11 @@
+export default class Card {
+  constructor({ name, link }, cardSelector) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+  }
+
+  _setEventListerners() {}
+
+  getView() {}
+}
