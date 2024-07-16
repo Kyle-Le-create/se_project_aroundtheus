7f6a0f6980ceeed26beab3a45c794a04b5a1c8bb
Kyle Le <158103657+Kyle-Le-create@users.noreply.github.com>
2024-07-16 11:31:33 -0500

    New update for my project 9

---
+++ src/components/Api.js
@@ -0,0 +1,7 @@
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "84b22c1c-9409-4095-8973-0de1c9a513f2",
+    "Content-Type": "application/json",
+  },
+});
